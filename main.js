576f716bf5ccb25388922426d76db317fad4432c
i.gorovoy <igor@kuna.family>
2022-07-29 15:27:02 +0200

    add page

---
+++ main.js
@@ -0,0 +1,6 @@
+// design source
+// https://www.vectorstock.com/royalty-free-vector/fashion-portrait-of-a-model-girl-with-sunglasses-vector-22319637
+
+// speed code video
+// https://twitter.com/asyrafhussin4/status/1544623076863664128
+//alert('You successfully linked your JavaScript file!');
